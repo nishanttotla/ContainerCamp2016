81802479a4371e620e07d8c4207b01b2b9fcbd2b
Nishant Totla <nishanttotla@gmail.com>
2016-09-07 10:29:46 -0700

    Adding ip address finder to demo
    Signed-off-by: Nishant Totla <nishanttotla@gmail.com>

---
+++ demo/magic.js
@@ -0,0 +1,42 @@
+// Source: http://stackoverflow.com/questions/391979/how-to-get-clients-ip-address-using-javascript-only/32841164#32841164
+
+function findIP(onNewIP) { //  onNewIp - your listener function for new IPs
+  var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
+  var pc = new myPeerConnection({iceServers: []}),
+    noop = function() {},
+    localIPs = {},
+    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    key;
+
+  function ipIterate(ip) {
+    if (!localIPs[ip]) onNewIP(ip);
+    localIPs[ip] = true;
+  }
+  pc.createDataChannel(""); //create a bogus data channel
+  pc.createOffer(function(sdp) {
+    sdp.sdp.split('\n').forEach(function(line) {
+      if (line.indexOf('candidate') < 0) return;
+      line.match(ipRegex).forEach(ipIterate);
+    });
+    pc.setLocalDescription(sdp, noop, noop);
+  }, noop); // create offer and set local description
+  pc.onicecandidate = function(ice) { //listen for candidate events
+    if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+    ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
+  };
+}
+
+
+
+var ul = document.createElement('ul');
+ul.textContent = 'IPs found for this container: '
+document.body.appendChild(ul);
+
+function addIP(ip) {
+  console.log('got ip: ', ip);
+  var li = document.createElement('li');
+  li.textContent = ip;
+  ul.appendChild(li);
+}
+
+findIP(addIP);
